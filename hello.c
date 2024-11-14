e6b3ceff6b904a0b89984ca40244ae6d97256201
Lea Devoudelle-Royer <ldevoude@z1r11p2.42lyon.fr>
2024-11-14 16:16:01 +0100

    test for deleti

--- hello.c
+++
@@ -1,4 +0,0 @@
-Piscines pédagogiques
-La prochaine session de piscines pédagogiques sera du 2 au 15 décembre 2024.
-Elles sont réservées aux Members.
-Vous avez jusqu'à lundi 18 novembre au soir pour nous donner vos réponses.
