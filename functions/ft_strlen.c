ec16d710424f6e3809949b521c621bc16c0f365c
Lea Devoudelle-Royer <ldevoude@z1r11p2.42lyon.fr>
2024-11-14 13:14:34 +0100

    {debug} fixed ft_strlen and norminette is ok

--- functions/ft_strlen.c
+++ functions/ft_strlen.c
@@ -6,32 +6,33 @@
 /*   By: ldevoude <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 15:10:20 by ldevoude          #+#    #+#             */
-/*   Updated: 2024/11/14 11:32:33 by ldevoude         ###   ########.fr       */
+/*   Updated: 2024/11/14 13:13:04 by ldevoude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <ctype.h>
+//#include <stdio.h>
+//#include <ctype.h>
+//#include <string.h>
 
 int	ft_strlen(char str[]);
-
+/*
 int	main(void)
 {
-	char	str[50] = "forty-two";
-	printf("origin : %s\n ft : %s\n",strlen(str), ft_strlen(str));
+	char	str[42] = "forty-two";
+	printf("origin : %lu\n ft : %i\n",strlen(str), ft_strlen(str));
 
 	return(0);
 }
+*/
 
-
-int	ft_strlen(char [])
+int	ft_strlen(char str[])
 {
 	int	i;
 	
 	i = 0;
-	while (str[i+1] != '\0') 
+	while (str[i] != '\0') 
 	{ 
-		i++
+		i++;
 	}
-	return (i+1);
+	return (i);
 }
