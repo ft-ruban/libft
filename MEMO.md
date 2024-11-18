f80370cc1a38cd4ea204f91645bdb19e2d4ca94c
Lea Devoudelle-Royer <ldevoude@z1r11p4.42lyon.fr>
2024-11-18 11:11:14 +0100

    {feat} finished strlcpy + norminette {update} updated the MEMO.md with task I have to do at some point

--- MEMO.md
+++ MEMO.md
@@ -3,4 +3,11 @@
   find where was my mistakes. (I did use too much * when I just created the pointers, check ft_memcpy.c for more
   info) I also find out a way to make a better while function : while (n--) dest_char++ =src_char++;
 
+###about memmove
+- variable that is unused and useless have to correct it at some point
+
+###about strlcpy
+- have to verify a bit more if everything is in order. it seems to be the case but I wanna be sure before getting an evaluation and maybe make my code more compact :>
+
+
 
---
+++ functions/ft_strlcpy.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldevoude <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 08:58:50 by ldevoude          #+#    #+#             */
+/*   Updated: 2024/11/18 11:09:43 by ldevoude         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+//#include <string.h>
+//#include <bsd/string.h> //-lbsd
+//#include <unistd.h>
+//#include <stdlib.h>
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+/*
+int	main(void)
+{
+	char dst[42] = "forty-two";
+	const char src[42] = "skibidi";
+	size_t size = 5;
+	int i = 0;
+
+	while (dst[i] != '\0')
+	{
+	printf("ft = %c ft_size = %zu\n",dst[i],ft_strlcpy(dst,src,size));
+	i++;
+	}
+	char dst2[42] = "forty-two";
+	const char src2[42] = "skibidi";
+	size = 5;
+	i = 0;
+
+	while (dst2[i] != '\0')
+	{
+	printf("normal = %c size = %zu\n",dst2[i],strlcpy(dst2,src2,size));
+	i++;
+	}
+	return 0;
+}
+*/
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	return_val;
+
+	return_val = 0;
+	while (--size > 0 && src[return_val] != '\0')
+	{
+		dst[return_val] = src[return_val];
+		return_val++;
+	}
+	dst[return_val] = '\0';
+	while (src[return_val] != '\0')
+		return_val++;
+	return (return_val);
+}
