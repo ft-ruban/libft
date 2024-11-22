345a364e059e3df950ccbf179a0aa59a4f13396a
Lea Devoudelle-Royer <ldevoude@z2r10p2.42lyon.fr>
2024-11-22 13:12:46 +0100

    {feat} finished ft_substr.c with norme

---
+++ functions/ft_substr.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldevoude <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/22 11:59:16 by ldevoude          #+#    #+#             */
+/*   Updated: 2024/11/22 13:12:07 by ldevoude         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdlib.h>
+//#include <stdio.h>
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ptr;
+	int		i;
+
+	i = 0;
+	ptr = malloc(sizeof(char) * len);
+	while (len-- > 0)
+	{
+		ptr[i] = s[start];
+		start ++;
+		i++;
+	}
+	ptr [start] = 0;
+	return (ptr);
+}
+/*
+int main(void)
+{
+	char s[42] = "forty-two";
+	int  start = 3;
+	size_t len = 5;
+	char *result;
+
+	//result = malloc(5);
+	result = ft_substr(s,start,len);
+
+//	while (result)
+	
+	{
+		printf("%s\n",result);
+//		result++;
+	}
+}*/
