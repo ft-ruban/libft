c0fef6156f2b3068420cdb56419cbca88da5849d
Lea Devoudelle-Royer <ldevoude@z2r10p2.42lyon.fr>
2024-11-22 11:08:11 +0100

    {norme} finished ft_strdup.c

--- functions/ft_strdup.c
+++ functions/ft_strdup.c
@@ -1,20 +1,32 @@
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <ctype.h>
-char *ft_strdup (const char *source)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldevoude <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/22 10:48:44 by ldevoude          #+#    #+#             */
+/*   Updated: 2024/11/22 11:07:20 by ldevoude         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <string.h>
+//#include <stdlib.h>
+//#include <stdio.h>
+//#include <ctype.h>
+char	*ft_strdup(const char *source)
 {
-    char *result;
-    size_t len;
+	char	*result;
+	size_t	len;
 
-    len = strlen(source) + 1;
-    result = malloc (len * sizeof(char));
-    if(!result)
-        return (0);
-    memcpy(result, source, len);
-    return (result);
+	len = strlen(source) + 1;
+	result = malloc (len * sizeof(char));
+	if (!result)
+		return (0);
+	memcpy (result, source, len);
+	return (result);
 }
-
+/*
 int main (void)
 {
     const char *original = "The original string.";
@@ -32,4 +44,4 @@ int main (void)
     free(copy);
 
     return 0;
-}
+}*/
