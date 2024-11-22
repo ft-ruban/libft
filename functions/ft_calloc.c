6804790634592cd956cccd360d7e6046647f3c74
Lea Devoudelle-Royer <ldevoude@z2r10p2.42lyon.fr>
2024-11-22 10:47:04 +0100

    {norm] finished norm for ft_calloc.c '

--- functions/ft_calloc.c
+++ functions/ft_calloc.c
@@ -1,27 +1,37 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldevoude <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/22 10:42:27 by ldevoude          #+#    #+#             */
+/*   Updated: 2024/11/22 10:45:49 by ldevoude         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void *ft_calloc(size_t element_count, size_t element_size)
+//#include <stdio.h>
+//#include <unistd.h>
+//#include <stdlib.h>
+
+void	*ft_calloc(size_t element_count, size_t element_size)
 {
-    char *ptr;
+	char	*ptr;
 
-    ptr = 0;
-    if (element_count == 0 || element_size == 0)
-        return (0);
-
-    ptr = malloc(sizeof(size_t) * (element_size));
-    if (ptr == 0)
-        return (0);
-
-    while (element_count--)
-    {
-        *ptr = 0;
-        ptr++;
-    }
-    return(ptr);
+	ptr = 0;
+	if (element_count == 0 || element_size == 0)
+		return (0);
+	ptr = malloc(sizeof(size_t) * (element_size));
+	if (ptr == 0)
+		return (0);
+	while (element_count--)
+	{
+		*ptr = 0;
+		ptr++;
+	}
+	return (ptr);
 }
-
+/*
 int main(void)
 {
     int num_rats = 5;
@@ -33,4 +43,4 @@ int main(void)
 
     while (num_rats--)
         printf("[%d]", rats_list[num_rats]);
-}
+}*/
