c51606bd0b59f77a3b0cf0206208045b834b8d6e
allarkpet <fishygayming@gmail.com>
2024-11-27 12:02:09 +0100

    {update} ft_itoa.c finished and works with all the possible int values. still need to do the norme

--- functions/ft_itoa.c
+++ functions/ft_itoa.c
@@ -10,80 +10,66 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-/* - while loop to detect the negative sign + size allocate with malloc
-
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-char *count_for_malloc(int *integer, int *is_negative);
+char *convert(long n,  int i, char *result, char *buff);
+char *count_for_malloc(long *integer, int *is_negative);
 
 char *ft_itoa(int n)
 {
 	char *result;
 	int i;
-	int j;
-	char *testi;
-	int lenght;
+	char *buff;
+    long ln;
 
 	i = 0;
-	if (n == 0 || n == -2147483648)
-	{
-		result = exceptionnal_cases(n);
-		if(!result)
-			return (0);
-		return (result);
-	}
-	testi = count_for_malloc(&n ,&i);
-	result = count_for_malloc(&n,&i);
+    ln = (long) n;
+    if (n == 0)
+    {
+        result = malloc(1 + 1 * sizeof(char));
+        result[0] = '0';
+        result[1] = 0;
+        return (result);
+    }
+	buff = count_for_malloc(&ln ,&i);
+	result = count_for_malloc(&ln,&i);
 	if(!result)
 		return (0);
-	j = i;
-	lenght = i;
-	printf("n: %d\n j = %d\n i = %d \n",n, i, j);
-	//printf("result : %s\n",result);
-	//if for the 0 case
-	while (n > 0)
-	{
-		testi[i] = n % 10 + 48;
-		n /= 10;
-		//printf("%c", testi[i]);
-		i++;
-	}
-	i--;
-	while (i != lenght - 1)
-	{
-		result[j++] = testi[i--];
-		//i--;
-		//j++;
-	}
+    if(!buff)
+        return (0);
+    result = convert (ln, i, result, buff);
 	return (result);
 }
 
-char *exceptionnal_cases(n);
+char *convert(long n,  int i, char *result, char *buff)
 {
-	char *ptr;
-	if (n == 0)
+	int length;
+    int j;
+
+    length = i;
+    j = i;
+	while (n > 0)
 	{
-		malloc(1 * sizeof(char));
-		if(!malloc)
-			return (0);
-		ptr[0] = '0';
-		return (ptr);
+		buff[i] = n % 10 + 48;
+		n /= 10;
+		i++;
 	}
-	else 
+	i--;
+	while (i != length - 1)
 	{
-		
+		result[j++] = buff[i--];
 	}
-	
+    result[j] = 0;
+	return (result);
 }
 
-char *count_for_malloc(int *integer,int *is_negative)
+char *count_for_malloc(long *integer,int *is_negative)
 {
 	char *result;
 	int	  count;
-	int	  buff;
+	long	  buff;
 
 	count = 0;
 	buff = *integer;
@@ -105,15 +91,12 @@ char *count_for_malloc(int *integer,int *is_negative)
 		return 0;
 	if(is_negative)
 		result[0] = '-';
-	//printf("fincount\n");
-	//printf("%d", *integer);
 	return (result);
 }
 
 int main (void)
 {
-	int n = 0;
+	int n = -42;
 	char *ptr = ft_itoa(n);
-	//ft_itoa(n);
 	printf("FINAL : %s", ptr);
 }
