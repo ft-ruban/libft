401c9e091081aad5648bf0ca892881e56507b45f
Lea Devoudelle-Royer <ldevoude@z1r11p4.42lyon.fr>
2024-12-03 10:52:24 +0100

    {feat} finished prototype of ft_putnbr.c, need to finish it by using my ft_itoa.c to convert the int into an ascii

---
+++ functions/ft_putnbr_fd.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldevoude <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/03 08:59:11 by ldevoude          #+#    #+#             */
+/*   Updated: 2024/12/03 10:51:00 by ldevoude         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	int	length;
+	char *itoa_result;
+
+	itoa_result = itoa //besoin de finir cette partie
+	length = strlen(itoa_result);
+	write (fd, itoa_result, length);
+	return ;
+}
+/*
+int main(void)
+{
+    int handle = open("test.txt", O_WRONLY);
+    char s[64] = "forty-two";
+    ft_putendl_fd(s, handle);
+    printf("Success!\n");
+	return (0);
+}
+*/
