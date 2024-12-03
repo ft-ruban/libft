612baab6a81ffbf69844e1173db59663940bc14b
Lea Devoudelle-Royer <ldevoude@z1r11p4.42lyon.fr>
2024-12-03 10:45:43 +0100

    {feat} finished+norme ft_putendl_fd.c

--- functions/ft_putendl_fd.c
+++ functions/ft_putendl_fd.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ldevoude <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 08:59:11 by ldevoude          #+#    #+#             */
-/*   Updated: 2024/12/03 10:39:58 by ldevoude         ###   ########.fr       */
+/*   Updated: 2024/12/03 10:43:13 by ldevoude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,13 @@
 #include <fcntl.h>
 #include <string.h>
 
-void	ft_putstr_fd(char *s, int fd)
+void	ft_putendl_fd(char *s, int fd)
 {
 	int	length;
 
 	length = strlen(s);
 	write (fd, s, length);
+	write (fd, "\n", 1);
 	return ;
 }
 /*
@@ -28,8 +29,8 @@ int main(void)
 {
     int handle = open("test.txt", O_WRONLY);
     char s[64] = "forty-two";
-    ft_putstr_fd(s, handle);
-    printf("Success!");
+    ft_putendl_fd(s, handle);
+    printf("Success!\n");
 	return (0);
 }
 */
