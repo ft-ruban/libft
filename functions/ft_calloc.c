465082a44d4a51df833c0a5f8452a9deffb50482
Lea Devoudelle-Royer <ldevoude@z1r11p4.42lyon.fr>
2024-12-05 15:34:07 +0100

    {fix} Fixed ft_malloc.c

--- functions/ft_calloc.c
+++ functions/ft_calloc.c
@@ -6,40 +6,41 @@
 /*   By: ldevoude <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 10:42:27 by ldevoude          #+#    #+#             */
-/*   Updated: 2024/12/03 13:04:48 by ldevoude         ###   ########.fr       */
+/*   Updated: 2024/12/05 15:29:52 by ldevoude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
-//#include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 
 void	*ft_calloc(size_t element_count, size_t element_size)
 {
 	char	*ptr;
+	size_t	i;
+	size_t	total_size;
 
-	ptr = 0;
-	if (element_count == 0 || element_size == 0)
-		return (0);
-	ptr = malloc(sizeof(size_t) * (element_size));
+	total_size = element_count * element_size;
+	i = 0;
+	ptr = malloc(total_size);
 	if (ptr == 0)
-		return (0);
-	while (element_count--)
+		return (NULL);
+	while (i != total_size)
 	{
-		*ptr = 0;
-		ptr++;
+		ptr[i] = 0;
+		i++;
 	}
 	return (ptr);
 }
 /*
 int main(void)
 {
-    int num_rats = 5;
+    int num_rats = 10;
     int *rats_list = 0;
     //int i;
 
 
-    rats_list = ft_calloc(num_rats, sizeof(int));
+    rats_list = ft_calloc(num_rats, 8);
 
     while (num_rats--)
         printf("[%d]", rats_list[num_rats]);
