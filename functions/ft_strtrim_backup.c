b884786d9e3c2bfb4064e8a360c68552a9243c1b
Lea Devoudelle-Royer <ldevoude@z1r11p4.42lyon.fr>
2024-12-06 09:16:27 +0100

    {fix} Fixed ft_strtrim.c and it is now entirely working

--- functions/ft_strtrim_backup.c
+++ functions/ft_strtrim_backup.c
@@ -6,7 +6,7 @@
 /*   By: ldevoude <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 15:36:02 by ldevoude          #+#    #+#             */
-/*   Updated: 2024/12/06 08:34:57 by ldevoude         ###   ########.fr       */
+/*   Updated: 2024/12/06 08:08:09 by ldevoude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,25 @@ char	*result(char const *s1, size_t start, size_t end);
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
+	char  *ptr;
 	size_t	i;
 	size_t	j;
 
-	
-	if (!s1 || !set)
-		return (NULL);
 	i = 0;
-	j = strlen(s1);
-	if (j == 0)
-		return strdup("");
-	while (i < j && trim(set, s1[i]))
+	ptr = malloc(sizeof(char) * 1);
+	if (!ptr)
+		return (NULL);
+	ptr[0] = 0;
+	if (!s1)
+		return (ptr);
+	j = strlen(s1) - 1;
+	if (strlen(s1) == 0)
+		return (0);
+	while (trim(set, s1[i]))
 		i++;
-	while (j > i && trim(set, s1[j - 1]))
+	while (trim(set, s1[j]))
 		j--;
-	return (result(s1, i, j));
+	return (result(s1, i, j - (i - 1)));
 }
 
 int	trim(const char *set, char c)
@@ -53,17 +57,23 @@ int	trim(const char *set, char c)
 char	*result(char const *s1, size_t start, size_t end)
 {
 	char	*str;
-	size_t len;
+	size_t	i;
 
-	len = end - start;
-	str = malloc(sizeof(char) * (len + 1));
+	i = 0;
+	if (end <= 0 || start >= strlen(s1))
+		return (0);
+	str = malloc(sizeof(char) * end + 1);
 	if (!str)
-		return (NULL);
-	strncpy(str, s1 + start, len);
-	str[len] = '\0';
+		return (0);
+	while (i < end)
+	{
+		str[i] = s1[start + i];
+		i++;
+	}
+		str[i] = 0;
 	return (str);
 }
-/*
+
 int main(void)
 {
     char s1[64] = "  \t \t \n   \n\n\n\t";
@@ -74,4 +84,4 @@ int main(void)
 
     return 0;
 }
-*/
+
